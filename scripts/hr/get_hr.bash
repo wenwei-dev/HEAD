07305bdcaf2e61dac1d96056bb6242159b6b9883
Wenwei Huang <dut.hww@gmail.com>
2017-04-10 16:17:44 +0800

    New version format

--- scripts/hr/get_hr.bash
+++ scripts/hr/get_hr.bash
@@ -3,6 +3,11 @@
 trap 'rm -f /tmp/hr' SIGINT SIGTERM EXIT
 
 get_hr() {
+    if hash hr 1>/dev/null 2>&1; then
+        echo "hr has already been installed."
+        echo "Run 'hr update hr' to update it."
+        exit
+    fi
     local url=https://raw.githubusercontent.com/hansonrobotics/HEAD/master/scripts/hr/hr
     local file=/tmp/hr
     curl -sLo $file ${url}
@@ -11,9 +16,8 @@ get_hr() {
         echo "Can't get hr"
         exit 1
     fi
-    chmod +x /tmp/hr
-    /tmp/hr install hr
-    rm -f /tmp/hr
+    bash $file install hr
+    rm -f $file
 }
 
 get_hr
